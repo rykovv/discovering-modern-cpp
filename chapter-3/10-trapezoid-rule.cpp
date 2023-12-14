6e18212f15c9ef7a29e2df55c1166ca7c72e16ef
Vladislav Rykov <rykovinternational@gmail.com>
2023-12-13 20:31:02 -0800

    regular solution

--- chapter-3/10-trapezoid-rule.cpp
+++ chapter-3/10-trapezoid-rule.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <cmath>
 
-
 template <unsigned N, typename F, typename T>
 class trapezoid_sum {
 
@@ -50,6 +49,34 @@ T trapezoid(const F& f, const T& a, const T& b) {
 }
 
 
+template <unsigned N, typename F, typename T>
+class trapezoid_sum_reg {
+public:
+
+    trapezoid_sum_reg(const F& f, const T& a, const T& h)
+      : f{f}, a{a}, h{h} 
+    {}
+
+    T operator() () const {
+        T sum = 0.0;
+        for (unsigned j = 1; j <= N; j++) {
+            sum += f(a + j*h);
+        }
+        return sum;
+    }
+
+private:
+    T a, h;
+    const F& f;
+};
+
+template <unsigned N, typename F, typename T>
+T trapezoid_reg(const F& f, const T& a, const T& b) {
+    T h = (b - a) / N;
+    return h*(f(a) + f(b))/2 + h*trapezoid_sum_reg<N-1, F, T>{f, a, h}();
+}
+
+
 double exp3f(double x) {
     return std::exp(3.0 * x);
 }
@@ -62,23 +89,34 @@ double mysincos(double x) {
     return x < 1 ? std::sin(x) : std::cos(x);
 }
 
+double mysin(double x) {
+    return std::sin(x);
+}
+
 
 int main() {
     auto i1 = trapezoid<50>(exp3f, 0.0, 4.0);
-    
+    std::cout << "trapezoid<50>(exp3f, 0.0, 4.0) = " << i1 << std::endl;
+    i1 = trapezoid_reg<50>(exp3f, 0.0, 4.0);
+    std::cout << "trapezoid_reg<50>(exp3f, 0.0, 4.0) = " << i1 << std::endl;
+
     exp3t exp3ts;
     auto i2 = trapezoid<50>(exp3ts, 0.0, 4.0);
-
-    std::cout << "trapezoid<50>(exp3f, 0.0, 4.0) = " << i1 << std::endl;
     std::cout << "trapezoid<50>(exp3ts, 0.0, 4.0) = " << i2 << std::endl;
+    i2 = trapezoid_reg<50>(exp3ts, 0.0, 4.0);
+    std::cout << "trapezoid_reg<50>(exp3ts, 0.0, 4.0) = " << i2 << std::endl;
 
     auto i3 = trapezoid<50>(mysincos, 0.0, 4.0);
-
     std::cout << "trapezoid<50>(mysincos, 0.0, 4.0) = " << i3 << std::endl;
+    i3 = trapezoid_reg<50>(mysincos, 0.0, 4.0);
+    std::cout << "trapezoid_reg<50>(mysincos, 0.0, 4.0) = " << i3 << std::endl;
 
 
     // how do I pass in a pointer to the right overloaded function?
-    // auto i4 = trapezoid<50>(std::sin, 0.0, 2.0);
+    // wrap it into a proxy function that will select the right 
+    //   overload by arg type
+    auto i4 = trapezoid<50>(mysin, 0.0, 2.0);
+    std::cout << "trapezoid<50>(mysin, 0.0, 2.0) = " << i4 << std::endl;
 
     return 0;
 }
