9468fb7c59872f098f64224ece21110c65f78a7f
Vladislav Rykov <rykovinternational@gmail.com>
2023-12-19 19:40:42 -0800

    trapezoid rule complete

--- chapter-3/10-trapezoid-rule.cpp
+++ chapter-3/10-trapezoid-rule.cpp
@@ -76,7 +76,30 @@ T trapezoid_reg(const F& f, const T& a, const T& b) {
     return h*(f(a) + f(b))/2 + h*trapezoid_sum_reg<N-1, F, T>{f, a, h}();
 }
 
+template <typename F, typename T>
+class finite_difference {
+public:
 
+    finite_difference(const F& f, const T& h)
+      : f{f}, h{h} 
+    {}
+
+    T operator() (const T& x) const {
+        return ( f(x+h) - f(x) ) / h;
+    }
+
+private:
+    const F& f;
+    T h;
+};
+
+template <typename F, typename T>
+auto finite_difference_proxy(const F& f, const T& h) {
+    finite_difference<F, T> fd{f, h};
+    return fd;
+}
+
+// referring std and passing it in as a pointer is undefined behavior
 double exp3f(double x) {
     return std::exp(3.0 * x);
 }
@@ -111,12 +134,29 @@ int main() {
     i3 = trapezoid_reg<50>(mysincos, 0.0, 4.0);
     std::cout << "trapezoid_reg<50>(mysincos, 0.0, 4.0) = " << i3 << std::endl;
 
-
     // how do I pass in a pointer to the right overloaded function?
     // wrap it into a proxy function that will select the right 
     //   overload by arg type
     auto i4 = trapezoid<50>(mysin, 0.0, 2.0);
     std::cout << "trapezoid<50>(mysin, 0.0, 2.0) = " << i4 << std::endl;
+
+    // integrating the finite difference
+    double p = 0.5;     // point
+    double h = 0.0001;
+    // check function value at .5
+    std::cout << "mysin(0.5) = " << mysin(0.5) << std::endl;
+    // get the slope at that point
+    // h of the finite difference is the delta 
+    auto findif = finite_difference_proxy(mysin, h);
+    std::cout << "findif(mysin, " << h << ")(" << p-h << ") = " << findif(p-h) << std::endl;
+    // integrate the derivative function at the previous point
+    auto i5 = trapezoid<50>(findif, p-h, p);
+    std::cout << "trapezoid<50>(findif, " << p-h << ", " << p << ") = " << i5 << std::endl;
+
+    // this gives the function value back, as h works as an approximation of the derivation step
+    auto i6 = trapezoid<50>(mysin, p-h, p);
+    std::cout << i6/h << std::endl;
+
 
     return 0;
 }
