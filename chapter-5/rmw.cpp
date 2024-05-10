1eb369dc6c383e5eb474562204beabd3632ee979
Vladislav Rykov <rykovinternational@gmail.com>
2024-05-10 11:11:19 -0700

    reg progress, tackling needs_read

--- chapter-5/rmw.cpp
+++ chapter-5/rmw.cpp
@@ -17,6 +17,8 @@ concept FieldSelectable = requires {
 template <typename T, unsigned msb, unsigned lsb>
 requires FieldSelectable<T, msb, lsb>
 struct rmw {
+    using value_type = T;
+
     static constexpr T mask = []() {
         if (msb != lsb) {
             if (msb == std::numeric_limits<T>::digits-1) {
@@ -34,25 +36,36 @@ struct rmw {
     }
 };
 
-template <typename T>
+template <typename ...Ts>
 concept MaskTrait = requires {
-    T::mask;
+    (Ts::mask,...);
 };
 
-template <typename ...Fields>
-struct rmw_register {};
+template <typename T, typename ...Ts>
+concept OfSameValueType = (std::same_as<typename T::value_type, typename Ts::value_type> && ...);
 
-template <typename Field>
-requires MaskTrait<Field>
-struct rmw_register<Field> {
-    Field field;
+template <typename T, typename ...Ts>
+concept RmwAble = MaskTrait<T, Ts...> && OfSameValueType<T, Ts...>;
+
+template <typename Field0, typename ...Fields>
+requires RmwAble<Field0, Fields...>
+struct rmw_fields {
+    using value_type = typename Field0::value_type;
 };
 
-template <typename Field, typename ...Fields>
-requires MaskTrait<Field>
-struct rmw_register<Field, Fields...> {
-    Field field;
-    rmw_register<Fields...> rest;
+template <typename F0, typename ...Fs>
+struct rmw_reg {
+    using value_type = typename F0::value_type;
+    static constexpr value_type layout = (F0::mask | ... | Fs::mask);
+
+    static constexpr bool needs_read (value_type new_value) {
+        return ((F0::mask & new_value > 0) && ... && (Fs::mask & new_value > 0));
+        // return ((F0::mask & new_value > 0) && ... && (Fs::mask & new_value > 0));
+    }
+
+    static constexpr value_type update (value_type old_value, value_type new_value) {
+        return (old_value & ~layout) | (new_value & layout);
+    }
 };
 
 int main() {
@@ -63,5 +76,15 @@ int main() {
     auto v = bar::update(0xFFFFFFFF, 0xF0BBF0F0);
     std::cout << std::hex << bar::mask << std::endl;
     std::cout << std::hex << v << std::endl;
+
+    using field0 = rmw<uint32_t, 4, 0>;
+    using field1 = rmw<uint32_t, 12, 8>;
+    using field2 = rmw<uint32_t, 20, 16>;
+    using field3 = rmw<uint32_t, 31, 28>;
+    using reg0 = rmw_reg<field0, field1, field2, field3>;
+    std::cout << std::hex << reg0::layout << std::endl;
+    std::cout << std::hex << reg0::needs_read(0xFFFF) << std::endl;
+    std::cout << std::hex << reg0::needs_read(0xFFFFFFFF) << std::endl;
+
     return v;
 }
