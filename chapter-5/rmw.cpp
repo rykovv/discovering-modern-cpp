8369d19dba8221a30e2f04cf052f79ea98d98be3
Vladislav Rykov <rykovinternational@gmail.com>
2024-05-28 23:18:26 -0700

    struggling with incomplete type error

--- chapter-5/rmw.cpp
+++ chapter-5/rmw.cpp
@@ -51,20 +51,38 @@ template <typename Field0, typename ...Fields>
 requires RmwAble<Field0, Fields...>
 struct rmw_fields {
     using value_type = typename Field0::value_type;
+    static constexpr value_type layout = (Field0::mask | ... | Fields::mask);
 };
 
-template <typename F0, typename ...Fs>
-struct rmw_reg {
-    using value_type = typename F0::value_type;
-    static constexpr value_type layout = (F0::mask | ... | Fs::mask);
+template <typename DerivedReg>
+struct rmw_reg;
 
-    static constexpr bool needs_read (value_type new_value) {
-        return !(((F0::mask & new_value) > 0) && (((Fs::mask & new_value) > 0) && ...));
-    }
+// Forward declaration
+struct derived_proxy;
+
+template <>
+struct rmw_reg<derived_proxy> {
+    using value_type = typename derived_proxy::Fields::value_type;
+    static constexpr value_type layout = derived_proxy::Fields::layout;
+
+    // static constexpr bool needs_read (value_type new_value) {
+    //     return !(((F0::mask & new_value) > 0) && (((Fs::mask & new_value) > 0) && ...));
+    // }
 
     static constexpr value_type update (value_type old_value, value_type new_value) {
         return (old_value & ~layout) | (new_value & layout);
     }
+};
+
+struct derived_proxy : rmw_reg<derived_proxy> {};
+
+struct my_reg : derived_proxy {
+    using field0 = rmw<uint32_t, 4, 0>;
+    using field1 = rmw<uint32_t, 12, 8>;
+    using field2 = rmw<uint32_t, 20, 16>;
+    using field3 = rmw<uint32_t, 31, 28>;
+
+    using Fields = rmw_fields<field0, field1, field2, field3>;
 };
 
 int main() {
@@ -76,14 +94,21 @@ int main() {
     std::cout << std::hex << bar::mask << std::endl;
     std::cout << std::hex << v << std::endl;
 
-    using field0 = rmw<uint32_t, 4, 0>;
-    using field1 = rmw<uint32_t, 12, 8>;
-    using field2 = rmw<uint32_t, 20, 16>;
-    using field3 = rmw<uint32_t, 31, 28>;
-    using reg0 = rmw_reg<field0, field1, field2, field3>;
-    std::cout << std::hex << reg0::layout << std::endl;
-    std::cout << std::hex << reg0::needs_read(0xFFFF) << std::endl;
-    std::cout << std::hex << reg0::needs_read(0xFFFFFFFF) << std::endl;
+
+    using RegFields = rmw_fields<
+        rmw<uint32_t, 4, 0>,
+        rmw<uint32_t, 12, 8>,
+        rmw<uint32_t, 20, 16>,
+        rmw<uint32_t, 31, 28>
+    >;
+    using reg0 = rmw_reg<RegFields>;
+    // std::cout << std::hex << reg0::layout << std::endl;
+
+    // reg0::field0::write(23);
+    // auto r = reg0::field0::read();
+
+    // std::cout << std::hex << reg0::needs_read(0xFFFF) << std::endl;
+    // std::cout << std::hex << reg0::needs_read(0xFFFFFFFF) << std::endl;
 
     return v;
 }
